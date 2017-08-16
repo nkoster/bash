22381881bbcd8c9227b19e4d5b683e8ae0690d25
Niels Koster <n.koster@portavita.eu>
2017-08-16 15:48:45 +0200

    Initial, find string(s) in repos

---
+++ find-string-in-git.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+me=`basename $0`
+
+if [ "$#" -ne 1 ]
+then
+	echo "$me usage: $me 'string'"
+    exit 1
+fi
+
+echo
+echo "Searching for \"${1}\""
+
+for repo in `find . -mindepth 1 -maxdepth 1 -type d`
+do
+	if [ -e ${repo}/.git/config ]
+	then
+		result=`egrep -lir $1 $repo | grep -v ^${repo}/.git`
+		if [ ! -z "$result" ]
+		then
+        	echo
+			echo "Git repository \"${repo}\" has a match for \"${1}\" in these files:"
+			echo "$result"
+			echo
+		fi
+	fi
+done
