819a6030d159dfa55d7078e4ab947b1d25f903a4
Niels Koster <n.koster@portavita.eu>
2017-08-17 11:35:08 +0200

    Add file counter

--- find-string-in-git.bash
+++ find-string-in-git.bash
@@ -15,13 +15,20 @@ for repo in `find . -mindepth 1 -maxdepth 1 -type d`
 do
 	if [ -e ${repo}/.git/config ]
 	then
-		result=`egrep -lir $1 $repo | grep -v ^${repo}/.git`
+		result=`egrep -lir "$1" "$repo" | grep -v "^${repo}/.git"`
 		if [ ! -z "$result" ]
 		then
+			count=$(echo "$result"|wc -l)
         	echo
-			echo "Git repository \"${repo}\" has a match for \"${1}\" in these files:"
-			echo "$result"
-			echo
+			echo -n "Git repository \"${repo}\" has a match for \"${1}\" in $count file"
+			[ $count -eq 1 ] || echo -n s
+			echo ":"
+			for n in "$result"
+			do
+				echo "$n"
+			done
 		fi
 	fi
 done
+
+echo
