616f4784e8e097b3bf73ba2dcfb9e17dd1ac16e3
Niels <nkoster@users.noreply.github.com>
2018-04-19 23:27:50 +0200

    Initial
    Run a JIRA JQL query against a JIRA API UTL

---
+++ jql.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Bugs by Niels, enjoy!
+
+function jql {
+
+url=https://$2/rest/api/2/search?jql=${1// /%20}
+curl -s -u $3:$4 \
+  -X GET -H "Content-Type: application/json" $url | \
+awk '/^\{/' | \
+jq '.' | \
+awk 'BEGIN{count=0}/^      "key":/{count++;printf("%s ", substr($2,2,length($2)-3))}END{print"("count")"}' 
+}
+
+if [ $# -lt 2 ]
+then
+  echo "Usage: $0 '<jql query> <fqdn[:port]> <user> <password file>"
+  echo "Remark: only https calls are supported."
+  exit 1
+fi
+
+jql "$1" "$2" "$3" "$4"
