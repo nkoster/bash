9d0b9d342bb447c54a3f07847b159d12f90520b2
Niels <nkoster@users.noreply.github.com>
2018-04-19 23:57:07 +0200

    Simplified

--- jql.bash
+++ jql.bash
@@ -5,18 +5,19 @@
 function jql {
 
 url=https://$2/rest/api/2/search?jql=${1// /%20}
-curl -s -u "$3":"$4" \
+curl -s -u "$3" \
   -X GET -H "Content-Type: application/json" $url | \
 awk '/^\{/' | \
 jq '.' | \
 awk 'BEGIN{count=0}/^      "key":/{count++;printf("%s ", substr($2,2,length($2)-3))}END{print"("count")"}' 
 }
 
-if [ $# -lt 4 ]
+if [ $# -lt 3 ]
 then
-  echo "Usage: $0 '\'<jql query>\' <fqdn[:port]> <user> <password>"
+  echo "Usage: $0 '<jql query>' <fqdn[:port]> '<user>:<password>'"
   echo "Remark: only https calls are supported."
+  echo $#
   exit 1
 fi
 
-jql "$1" "$2" "$3" "$4"
+jql "$1" "$2" "$3"
