dbafbc1f2cbc52f203dcec6c176d54a07a588efd
Niels Koster <n.koster@portavita.eu>
2018-07-19 02:02:59 +0200

    Local and remote port

--- gpgchat.bash
+++ gpgchat.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-[[ $# -lt 3 ]] && exit 1
+[[ $# -lt 4 ]] && exit 1
 
 user_remote="$1"
 host="$2"
-port="$3"
+port_local="$3"
+port_remote="$4"
 
 keys=$(gpg --list-keys "$user_remote" | grep -c '^$')
 if [ $keys -gt 1 ]; then
@@ -15,7 +16,7 @@ fi
 export GPG_TTY=$(tty)
 
 echo "Starting listener at port $port"
-while true ; do ncat -l 5000 | gpg -d --batch --passphrase "$(cat /home/niels/secure/hup3)" 2>/dev/null ; done &
+while true ; do ncat -l $port_local | gpg -d --batch --passphrase "$(cat /home/niels/secure/hup3)" 2>/dev/null ; done &
 
 echo "Initiating pgp chat to $user_remote at $host"
 echo
@@ -23,6 +24,6 @@ echo
 while true
 do
     read n
-    echo "$n" | gpg -e -r "$user_remote" | ncat $host $port
+    echo "$n" | gpg -e -r "$user_remote" | ncat $host $port_remote
 done
 
