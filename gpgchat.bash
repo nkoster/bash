94d55ba0289b2bef1f766c00be3924eb086acece
Niels Koster <n.koster@portavita.eu>
2018-07-19 02:04:08 +0200

    port_local bug

--- gpgchat.bash
+++ gpgchat.bash
@@ -15,7 +15,7 @@ fi
 
 export GPG_TTY=$(tty)
 
-echo "Starting listener at port $port"
+echo "Starting listener at port $port_local"
 while true ; do ncat -l $port_local | gpg -d --batch --passphrase "$(cat /home/niels/secure/hup3)" 2>/dev/null ; done &
 
 echo "Initiating pgp chat to $user_remote at $host"
