216bb409a42a1a4bd332eed40aa4ce1a7f1b47b9
Niels Koster <n.koster@portavita.eu>
2019-06-01 12:51:18 +0200

    Initial, generate web fonts with fontforge.

---
+++ webfont.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "error"
+    exit 1
+fi
+
+name=${1%%.*}
+extension=${1##*.}
+extension=${extension^^}
+
+echo "$name --- $extension"
+
+if [ ! -f "$1" ]; then
+    echo "noo $1"
+    exit 2
+fi
+
+mkdir -p "$name/fonts/$name"
+fontforge -c "import fontforge;fontforge.open('$1').generate('$name/fonts/$name/$name.ttf')"
+fontforge -c "import fontforge;fontforge.open('$1').generate('$name/fonts/$name/$name.eot')"
+fontforge -c "import fontforge;fontforge.open('$1').generate('$name/fonts/$name/$name.svg')"
+fontforge -c "import fontforge;fontforge.open('$1').generate('$name/fonts/$name/$name.woff')"
+cp $1 $name/fonts/$name/
+
+mkdir -p "$name/css/"
+echo "@font-face {" >$name/css/$name.css
+echo "    font-family: '$name';" >>$name/css/$name.css
+echo "    src: url('../fonts/$name/$name.eot');" >>$name/css/$name.css
+echo "    src: url('../fonts/$name/$name.eot?#iefix') format('embedded-opentype')," >>$name/css/$name.css
+echo "    url('../fonts/$name/$name.woff') format('woff')," >>$name/css/$name.css
+echo "    url('../fonts/$name/$name.ttf') format('truetype')," >>$name/css/$name.css
+echo "    url('../fonts/$name/$name.svg#lorabold') format('svg');" >>$name/css/$name.css
+echo "}" >>$name/css/$name.css
+
+cd "$name"
+zip -r ../$name.zip fonts/ css/
+cd ..
+rm -rf "$name"
