1d0758ae80e5321598c5489a1fef14c635a6d6b8
Niels Koster <n.koster@portavita.eu>
2019-06-02 01:17:26 +0200

    Use determined path.

--- webfont.bash
+++ webfont.bash
@@ -5,33 +5,35 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-name=${1%%.*}
+path=$(dirname $1)
+cd "$path"
+
+name=$(basename ${1%%.*})
 extension=${1##*.}
 extension=${extension^^}
-
 
 if [ ! -f "$1" ]; then
     echo "noo $1"
     exit 2
 fi
 
-mkdir -p "$name/fonts/$name"
-fontforge -c "import fontforge;fontforge.open('$1').generate('$name/fonts/$name/$name.ttf')"
-fontforge -c "import fontforge;fontforge.open('$1').generate('$name/fonts/$name/$name.eot')"
-fontforge -c "import fontforge;fontforge.open('$1').generate('$name/fonts/$name/$name.svg')"
-fontforge -c "import fontforge;fontforge.open('$1').generate('$name/fonts/$name/$name.woff')"
+mkdir -p "$path/$name/fonts/$name"
+fontforge -c "import fontforge;fontforge.open('$1').generate('$path/$name/fonts/$name/$name.ttf')"
+fontforge -c "import fontforge;fontforge.open('$1').generate('$path/$name/fonts/$name/$name.eot')"
+fontforge -c "import fontforge;fontforge.open('$1').generate('$path/$name/fonts/$name/$name.svg')"
+fontforge -c "import fontforge;fontforge.open('$1').generate('$path/$name/fonts/$name/$name.woff')"
 
-mkdir -p "$name/css/"
-echo "@font-face {" >$name/css/$name.css
-echo "    font-family: '$name';" >>$name/css/$name.css
-echo "    src: url('../fonts/$name/$name.eot');" >>$name/css/$name.css
-echo "    src: url('../fonts/$name/$name.eot?#iefix') format('embedded-opentype')," >>$name/css/$name.css
-echo "    url('../fonts/$name/$name.woff') format('woff')," >>$name/css/$name.css
-echo "    url('../fonts/$name/$name.ttf') format('truetype')," >>$name/css/$name.css
-echo "    url('../fonts/$name/$name.svg#lorabold') format('svg');" >>$name/css/$name.css
-echo "}" >>$name/css/$name.css
+mkdir -p $path/$name/css/
+echo "@font-face {" >$path/$name/css/$name.css
+echo "    font-family: '$name';" >>$path/$name/css/$name.css
+echo "    src: url('../fonts/$name/$name.eot');" >>$path/$name/css/$name.css
+echo "    src: url('../fonts/$name/$name.eot?#iefix') format('embedded-opentype')," >>$path/$name/css/$name.css
+echo "    url('../fonts/$name/$name.woff') format('woff')," >>$path/$name/css/$name.css
+echo "    url('../fonts/$name/$name.ttf') format('truetype')," >>$path/$name/css/$name.css
+echo "    url('../fonts/$name/$name.svg#lorabold') format('svg');" >>$path/$name/css/$name.css
+echo "}" >>$path/$name/css/$name.css
 
-cd "$name"
+cd "$path/$name"
 zip -r ../$name.zip fonts/ css/
 cd ..
-rm -rf "$name"
+echo "$(pwd) rm -rf \"$name\""
